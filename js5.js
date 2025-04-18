bc1a74a9e78b185a4a1b1d5b1cd4cc6305d6fbf1
1keshavrana <1keshavrana@gmail.com>
2025-04-18 12:38:51 +0530

    code-v

---
+++ js5.js
@@ -0,0 +1,8 @@
+let streng = prompt("Enter a String : ");
+
+if ((streng[0]=='a' || streng[0]=='A') && streng.length > 5 ){
+    alert("Entered string is Golden String >>>");
+}
+else{
+    alert("Not a Golden String >>>>");
+}
