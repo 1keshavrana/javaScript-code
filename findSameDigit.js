88d56f48ac3533d899805df97a3d0bd6ea6f3853
1keshavrana <1keshavrana@gmail.com>
2025-04-18 13:02:39 +0530

    code-vii

---
+++ findSameDigit.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Enter any number value : ");
+let num2 = prompt("Enter 2nd number value : ");
+
+if (num1 % 10 == num2 % 10){
+    alert("Both Number have same last digit >..")
+}
+else(
+    alert("Not have same last digit ...")
+)
