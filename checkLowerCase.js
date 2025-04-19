179258a3799252a41fe429935ac02e89094c8275
1keshavrana <1keshavrana@gmail.com>
2025-04-19 11:37:14 +0530

    code-11

---
+++ checkLowerCase.js
@@ -0,0 +1,8 @@
+let str = prompt("Enter string : ");
+let n = prompt("Enter the index : ");
+if (str[n] == str[n].toLowerCase()){
+    console.log("Character is Lower Case. ");
+}
+else{
+    console.log("Not Lower Case...");
+}
