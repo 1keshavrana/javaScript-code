1fbcae66dabf0a9157a937cdb78559683c57fba2
1keshavrana <1keshavrana@gmail.com>
2025-04-19 23:47:55 +0530

    code-22

---
+++ object.js
@@ -0,0 +1,6 @@
+let car = {
+    name : "TATA",
+    modal : "TIAGO",
+    color : "white" 
+};
+console.log(car.name);
---
+++ object2.js
@@ -0,0 +1,8 @@
+let person = {
+    name : "Keshav rana",
+    age : "20",
+    city : "Kalayat" 
+};
+person.city = "New York";
+person.country = "USA";
+console.log(person);
