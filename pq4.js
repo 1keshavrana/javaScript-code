5411f731dbfa994d3d3630463ae5b0ebbc7562da
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-10-21 13:18:01 +0530

    pq-4-p1

---
+++ pq4.js
@@ -0,0 +1,15 @@
+let name = prompt("Enter your Name : ");
+
+//to count the length of string ...
+let n = name.length;
+
+alert(`Length of your name is : ${n}`);
+
+
+//to print first letter of string ...
+
+alert(`First letter is  : ${name[0]}`);
+
+//to print last letter of string ...
+
+alert(`Last letter is : ${name[n-1]}`);
