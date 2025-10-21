033dfbaf984f1129f113cdeff790486ec78462d0
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-10-21 11:55:53 +0530

    pq-1-p1.js

---
+++ pq1.js
@@ -0,0 +1,7 @@
+let age = prompt("enter age : "); //  it's take input as string
+
+let age1 = 10; // it's an integer
+
+console.log(age + 10 ); // it's give (1010) output bcz of age is an string type ...
+
+console.log(age1 + 10); //it's give (20) output bcz of age1 is an integer type...
