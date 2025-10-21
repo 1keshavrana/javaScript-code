b1c2bce148143b0f995e79e25ca3326f2cfe37a8
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-10-21 12:14:59 +0530

    pq3.js

---
+++ pq3.js
@@ -0,0 +1,9 @@
+let num = Number(prompt("enter number : "));
+let newn  = num++;
+
+alert(`After first num++ or newn  : ${newn} and num is : ${num}`);
+
+
+newn = ++num;
+
+alert(`after 2nd ++num or newn : ${newn} and num is : ${num}`);
