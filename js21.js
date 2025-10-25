712be321d516689568cfd32214369ddb32c9c756
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-10-25 11:18:19 +0530

    js's

--- js21.js
+++ js21.js
@@ -9,13 +9,5 @@ inp.setAttribute("placeholder","username");
 btn.style.backgroundColor = 'blue';
 
 
-let h1=document.createElement("h1");
-h1.innerHTML="<u>DOMPractice</u>";
-document.querySelector("body").append(h1);
-
-let p=document.createElement("p");
-p.innerHTML="ApnaCollege<b>Delta</b>Practice";
-document.querySelector("body").append(p);
-
 document.querySelector('body').append(inp);
 document.querySelector('body').append(btn);
