116be1940e0fe7e3af7c9d85808645cb4af216e5
KESHAV RANA <140576247+1keshavrana@users.noreply.github.com>
2025-10-25 10:59:32 +0530

    js20-p9

---
+++ js20.js
@@ -0,0 +1,8 @@
+let input = document.createElement('input');
+let btn  =  document.createElement('button');
+
+input.placeholder = "enter you name";
+btn.innerText = "click me";
+
+document.querySelector('body').append(input);
+document.querySelector('body').append(btn);
